da99cc2a5e567b954f9998a8328a3cb64058e899
Felix Schreiber <felix.schreiber@auctores.de>
2024-09-16 11:34:28 +0200

    fixed macro statement

--- src/parse/parse_statement.cpp
+++ src/parse/parse_statement.cpp
@@ -2,6 +2,8 @@
 #include <QLang/Parser.hpp>
 #include <QLang/QLang.hpp>
 
+#include "QLang/Context.hpp"
+
 QLang::StatementPtr QLang::Parser::ParseStatement()
 {
     if (At(TokenType_CompileDirective))
@@ -15,6 +17,29 @@ QLang::StatementPtr QLang::Parser::ParseStatement()
     if (At("if")) return ParseIf();
     if (At("return")) return ParseReturn();
     if (At("while")) return ParseWhile();
+
+    if (At(TokenType_Name) && m_Context.HasMacro(m_State.Tok.Value))
+    {
+        if (const auto macro = m_Context.GetMacro(Skip().Value); macro.IsCallee)
+        {
+            std::vector<StatementPtr> args;
+
+            Expect("(");
+            while (!NextIfAt(")"))
+            {
+                args.push_back(ParseStatement());
+                if (!At(")"))
+                    Expect(",");
+            }
+
+            macro.Expand(*this, args);
+        }
+        else
+        {
+            macro.Expand(*this);
+        }
+        return ParseStatement();
+    }
 
     return ParseBinary();
 }
